3f18f184132eac8340ef220c56070afb1675a188
Yufei Sun <109019906+Siwi0w0@users.noreply.github.com>
2023-02-19 17:21:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const rating = document.getElementById("rating");
 const rates = document.querySelectorAll(".rtng-btn");
 
 sbmtBtn.addEventListener("click", function () {
+    if (rating.innerText === ''){
+        
+    } else {
         thankYou.classList.remove("hide");
         mainBox.classList.add("hide");
+    }
+        
     });
 
 rates.forEach(function (rate) {
